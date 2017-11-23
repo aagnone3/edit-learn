bddbd0bbeb25075437850f30a23f1d53e7ae830e
Anthony Agnone <aagnone@pindrop.com>
2017-11-23 15:02:21 -0500

    Checkpoint - WIP for parsing XMP files into feature vectors

---
+++ extract/extract.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# argument parsing
+N_ARGS=1
+if [ "$#" -lt ${N_ARGS} ]; then
+	echo "Usage: sh extract.sh <path-to-file-list>"
+	exit
+fi
+file_list=${1}
+
+XMP_HEADER_TAG=xpacket
+XMP_HEADER="<?xpacket begin=\"\" id=\"W5M0MpCehiHzreSzNTczkc9d\"?>"
+XMP_FOOTER="<?xpacket end=\"w\"?>"
+TMP_FILE=shtemp
+
+# detect an invalid file list.
+[ ! -e ${file_list} ] && {
+    echo "ERROR: File <${file_list}> does not exist."
+    exit
+} 
+
+function validate_xmp() {
+    fn=${1}
+	xpacket_present=`head -n1 ${fn} | grep -c ${XMP_HEADER_TAG}`
+	[ "${xpacket_present}" -eq "0" ] && {
+	    (echo ${XMP_HEADER}; cat ${fn}; echo ${XMP_FOOTER};) > ${TMP_FILE}
+	    mv ${TMP_FILE} ${fn}
+    }
+}
+
+while read -r fn; do
+    # handle Lightroom exporting XMP files without the necessary header and footer
+    validate_xmp ${fn}
+done < ${file_list}
+
