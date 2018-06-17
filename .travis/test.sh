435fbc72db5d6c7e8a2be8cb51f5458e289ca77d
Anthony Agnone <aagnone@pindrop.com>
2018-06-17 16:39:12 -0400

    Test updates.

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,9 +5,17 @@ PYTEST_OPTIONS=(--cov=${MODULE} -xvs --pyargs ${MODULE})
 COVERAGE_FILE=${TEST_DIR}/.coverage
 
 # ensure a clean temporary test directory
-[[ -d ${TEST_DIR} ]] && rm -rf ${TEST_DIR}
-cd ${TEST_DIR}
+function ensure_clean_test_dir() {
+    [[ -d ${TEST_DIR} ]] && rm -rf ${TEST_DIR}
+    mkdir -p ${TEST_DIR}
+}
 
-py.test "${PYTEST_OPTIONS[@]}" .
-[[ -f ${COVERAGE_FILE} ]] && cp ${COVERAGE_FILE} ${TRAVIS_BUILD_DIR} || echo "Warning: code coverage file not available."
-cd ${OLDPWD}
+function do_test() {
+    cd ${TEST_DIR}
+    py.test "${PYTEST_OPTIONS[@]}" .
+    [[ -f ${COVERAGE_FILE} ]] && cp ${COVERAGE_FILE} ${TRAVIS_BUILD_DIR} || echo "Warning: code coverage file not available."
+    cd ${OLDPWD}
+}
+
+ensure_clean_test_dir
+do_test
