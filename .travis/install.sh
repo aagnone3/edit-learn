81fbe1d658ea93e26ef4b03259009ffaa56127c4
Anthony Agnone <aagnone@pindrop.com>
2018-06-17 19:18:56 -0400

    Install libexempi3.

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,10 +5,5 @@ set -e
 pip install --upgrade pip
 pip install pytest pytest-cov codecov sphinx
 
-# install libexempi
-# TODO in a Docker container
-sudo apt update
-sudo apt install libexempi3
-
 # install the package
 pip install .
