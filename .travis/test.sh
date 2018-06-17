61bea0be967abdea5bf8a382e430046f54e45991
Anthony Agnone <aagnone@pindrop.com>
2018-06-17 17:20:22 -0400

    Test updates.

--- .travis/test.sh
+++ .travis/test.sh
@@ -13,18 +13,8 @@ function ensure_clean_test_dir() {
 function do_test() {
     cd ${TEST_DIR}
     PYTHONPATH=${PYTHONPATH}:${TRAVIS_BUILD_DIR} py.test "${PYTEST_OPTIONS[@]}" ${TRAVIS_BUILD_DIR}
-}
-
-function upload_coverage() {
-    cd ${TRAVIS_BUILD_DIR}
-    [[ -f ${COVERAGE_FILE} ]] && {
-        cp ${COVERAGE_FILE} ${TRAVIS_BUILD_DIR}
-        codecov || echo "Warning: code coverage upload failed."
-    } || {
-        echo "Warning: code coverage file not available."
-    }
+    cp ${COVERAGE_FILE} ${TRAVIS_BUILD_DIR}
 }
 
 ensure_clean_test_dir
 do_test
-upload_coverage
