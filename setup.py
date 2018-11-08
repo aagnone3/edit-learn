5967d03af20065f0be0f7e163646ed3ed62ca0d1
Anthony <anthonyagnone@gmail.com>
2018-11-07 20:54:28 -0500

    New setup process; added .gitignore

--- setup.py
+++ setup.py
@@ -1,35 +1,57 @@
 from os import path
+from glob import glob
 from setuptools import setup, find_packages
+from configparser import ConfigParser
 
-MODULE_NAME = "ielearn"
-VERSION_FN = path.join(MODULE_NAME, "version.py")
-REQUIREMENTS_FN = path.join(path.dirname(__file__), 'requirements.txt')
 
-# Read requirements
-with open(REQUIREMENTS_FN, 'r') as fh:
-    requirements = [str(x).strip() for x in fh.readlines()]
+def get_package_info():
+    cfg = ConfigParser()
+    cfg.read("setup.cfg")
+    return cfg["package_info"]
 
-# Read from and write to the version file
-with open(VERSION_FN) as fp:
-    version = float(fp.readlines()[0].strip().split('=')[1].replace(' ', ''))
 
-# Get list of data files
-data_files = ['README.rst']
+def get_requirements():
+    requirements_fn = path.join(path.dirname(__file__), 'requirements.txt')
+    with open(requirements_fn, 'r') as fh:
+        requirements = [str(x).strip() for x in fh.readlines()]
+    return requirements
+
+
+def get_version(module_name):
+    version_fn = path.join(module_name, "version.py")
+    with open(version_fn) as fp:
+        version = float(fp.readlines()[0].strip().split('=')[1].replace(' ', ''))
+    return version
+
+
+def get_data_files():
+    data_files = ['README.rst']
+    if path.isdir("conf"):
+        data_files.append([
+            path.join("conf", fn)
+            for fn in glob(path.join("conf", "*"))
+        ])
+    return data_files
+
+
+package_info = get_package_info()
+module_name = package_info["module_name"]
 
 setup(
-    name="img-edit-learn",
-    version=version,
-    description="Machine Learning for Personalized Image Editing",
+    name=package_info["friendly_name"],
+    version=get_version(package_info["module_name"]),
+    description=package_info["description"],
+    long_description=package_info.get("long_description", package_info["description"]),
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering"
     ],
-    url="https://github.com/aagnone3/img-edit-learn",
-    author="Anthony Agnone",
-    author_email="anthonyagnone@gmail.com",
+    url=package_info["url"],
+    author=package_info["author"],
+    author_email=package_info["author_email"],
     packages=find_packages(exclude=['*.test', 'test']),
-    install_requires=requirements,
+    install_requires=get_requirements(),
     zip_safe=False,
-    data_files=[('share/aagnone/ielearn', data_files)]
+    data_files=[('share/{}'.format(module_name), get_data_files())]
 )
